1d5af9d8cd82e13721ca799b1cfcfe2306df8705
Rick O'Donnell <rodonnell@myperfectgig.com>
2016-03-29 11:31:19 -0400

    Added toggle between all events and current event

--- reports/reportsScoutsStats.php
+++ reports/reportsScoutsStats.php
@@ -2,6 +2,9 @@
 include "../includes/sessionCheck.php";
 include "../includes/globalVars.php";
 
+$allEvents = ($_GET["allEvents"] == "true" || $_POST["allEvents"] == "true");
+$query = "SELECT * FROM ".$matchesTable." WHERE eventkey = '" . $currEvent . "' ORDER BY matchnumber ASC;";
+$query = "SELECT * FROM ".$matchesTable." WHERE eventkey = '" . $currEvent . "' ORDER BY matchnumber ASC;";
 ?>
 <!DOCTYPE html>
 <html>
@@ -15,14 +18,24 @@ include "../includes/globalVars.php";
         <div class="page-header"><h2>Reports - Scouts Stats</h2></div>
         
         <!-- Match Scouting -->
-        <div class="page-header"><h4>Match Scouting All-Stars</h4></div>
+        <div class="page-header">			
+            <?php if ($allEvents) { ?>
+                <h4>Match Scouting All-Stars - All Events (<a href="reportsScoutsStats.php?allEvents=false">see only <?= $currEventName ?></a>)</h4>
+            <?php } else { ?>
+                <h4>Match Scouting All-Stars - <?= $currEventName ?> (<a href="reportsScoutsStats.php?allEvents=true">see all events</a>)</h4>
+            <?php } ?>
+	    </div>
         <table class="table table-striped">
             <thead>
                 <tr><th scope="col">Scout</th><th scope="col"># Matches Scouted</th></tr>
             </thead>
             <tbody>    
             <?php
-            $query1 = "SELECT scout, COUNT(*) AS num_reports FROM ".$performancesTable." GROUP BY scout ORDER BY num_reports DESC;";
+            if ($allEvents) {
+                $query1 = "SELECT scout, COUNT(*) AS num_reports FROM ".$performancesTable." GROUP BY scout ORDER BY num_reports DESC;";
+	    } else {
+	        $query1 = "SELECT scout, COUNT(*) AS num_reports FROM ".$performancesTable." WHERE eventkey = '" . $currEvent . "' GROUP BY scout ORDER BY num_reports DESC;";
+	    }
             $result1 = $db->query($query1);
             while ($row = mysqli_fetch_assoc($result1)) {
             ?>
